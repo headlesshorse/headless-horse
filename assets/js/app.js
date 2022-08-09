7cc1ae499ad6b3f4e57fa97c2e29d37a5522de97
Headless Horse <78816417+Headless-Horse@users.noreply.github.com>
2022-08-09 19:21:20 +0100

    3.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -615,7 +615,7 @@ void(function(root, factory) {
     $dismiss.onclick = dismiss
     $div.appendChild($dismiss)
 
-    document.getElementById("terminal").after($div)
+    document.body.appendChild($div)
   }
 
   function ready(fn) {
