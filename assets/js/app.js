1e38e8fda5fd4ce8f54e8e55d7432ff6b96649fc
Headless Horse <78816417+Headless-Horse@users.noreply.github.com>
2023-04-26 19:53:25 +0100

    4.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -244,7 +244,7 @@ document.body.appendChild(cookienotice);
 function setCookie(cookieName, cookieValue, expirationDays) {
   const d = new Date();
   d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
-  const expires = "expires=" + d.toUTCString();
+  const expires = 'expires=' + d.toUTCString();
   document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
 }
 
