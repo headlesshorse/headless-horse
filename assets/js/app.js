1111a8685f6462ae6e34df88f24fdfdf3b485062
Headless Horse <78816417+Headless-Horse@users.noreply.github.com>
2023-05-04 12:38:37 +0100

    4.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -72,7 +72,7 @@ if (day === 0 || day === 6) {
 
 /***************************************** Read More *****************************************/
 const readMoreBtn = document.createElement('a');
-readMoreBtn.textContent = 'Read More.';
+readMoreBtn.textContent = `Read More.`;
 readMoreBtn.style.display = 'block';
 readMoreBtn.style.cursor = 's-resize';
 
@@ -81,12 +81,12 @@ readMoreText.insertAdjacentElement('afterend', readMoreBtn);
 readMoreText.style.display = 'none';
 
 readMoreBtn.addEventListener('click', () => {
-  if (readMoreBtn.textContent === 'Read Less.') {
-    readMoreBtn.textContent = 'Read More.';
+  if (readMoreBtn.textContent === `Read Less.`) {
+    readMoreBtn.textContent = `Read More.`;
     readMoreText.style.display = 'none';
     readMoreBtn.style.cursor = 's-resize';
   } else {
-    readMoreBtn.textContent = 'Read Less.';
+    readMoreBtn.textContent = `Read Less.`;
     readMoreText.style.display = 'block';
     readMoreBtn.style.cursor = 'n-resize';
   }
