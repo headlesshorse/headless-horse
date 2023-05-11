ad4dedf9fb391e5b960dec31a5f8c319aa28c628
Headless Horse <78816417+Headless-Horse@users.noreply.github.com>
2023-05-11 23:41:00 +0100

    4.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 /***************************************** Loader *****************************************/
 let loaderDiv = document.createElement('div');
-loaderDiv.setAttribute('id', 'loader');
+loaderDiv.id = 'loader';
 
 let loaderBarDiv = document.createElement('div');
-loaderBarDiv.setAttribute('id', 'loader--bar');
+loaderBarDiv.id = 'loader--bar';
 
 loaderDiv.appendChild(loaderBarDiv);
 document.body.appendChild(loaderDiv);
@@ -21,8 +21,7 @@ function frame() {
   if (loaderBarWidth >= width) {
     clearInterval(loaderInterval);
   } else {
-    loaderBarWidth++;
-    loaderBar.style.width = loaderBarWidth + '%';
+    loaderBar.style.width = (++loaderBarWidth) + '%';
   }
 }
 
@@ -62,7 +61,7 @@ previousSection.after(latestTitle);
 fetch("https://potion-api.now.sh/html?id=f97f1af964fe48989650aae62609bf37")
   .then(res => res.text())
   .then(text => {
-    latestTitle.setAttribute('id', 'latest');
+    latestTitle.id = 'latest';
     latestTitle.innerHTML = `Latest`;
     latestTitle.insertAdjacentHTML('afterend', text);
     document.querySelectorAll('#latest + ul li a').forEach(link => {
@@ -90,171 +89,127 @@ if (day === 0 || day === 6) {
 
 /***************************************** Read More *****************************************/
 const readMoreBtn = document.createElement('a');
-readMoreBtn.textContent = `Read More.`;
+readMoreBtn.textContent = 'Read More.';
 readMoreBtn.style.display = 'block';
 readMoreBtn.style.cursor = 's-resize';
 
-const readMoreTesat = document.querySelector('h1 + p');
-readMoreTesat.insertAdjacentElement('beforeend', readMoreBtn);
-
 const readMoreText = document.querySelectorAll('span');
-for (let i = 0; i < readMoreText.length; i++) {
-  readMoreText[i].style.display = 'none';
-}
+readMoreText.forEach(element => {
+  element.style.display = 'none';
+});
 
 readMoreBtn.addEventListener('click', () => {
-  if (readMoreBtn.textContent === `Read Less.`) {
-    readMoreBtn.textContent = `Read More.`;
-    readMoreBtn.style.cursor = 's-resize';
-    for (let i = 0; i < readMoreText.length; i++) {
-      readMoreText[i].style.display = 'none';
-    }
-  } else {
-    readMoreBtn.textContent = `Read Less.`;
-    readMoreBtn.style.cursor = 'n-resize';
-    for (let i = 0; i < readMoreText.length; i++) {
-      readMoreText[i].style.display = 'inline';
-    }
-  }
+  const isReadLess = readMoreBtn.textContent === 'Read Less.';
+  readMoreBtn.textContent = isReadLess ? 'Read More.' : 'Read Less.';
+  readMoreBtn.style.cursor = isReadLess ? 's-resize' : 'n-resize';
+
+  readMoreText.forEach(element => {
+    element.style.display = isReadLess ? 'none' : 'inline';
+  });
 });
+
+document.querySelector('h1 + p').insertAdjacentElement('beforeend', readMoreBtn);
 
 /***************************************** Wall Image *****************************************/
 var acceleration = 0.01;
-
-var img = {
-  element: document.querySelector('#wall-image'),
-  xMax: 0,
-  yMax: 0,
-  x: 0,
-  y: 0
-};
-
-var mouse = {
-  x: 0,
-  y: 0
-};
-
-var vw = 0;
-var vh = 0;
-
-var targetX = 0;
-var targetY = 0;
+var img = { element: document.querySelector('#wall-image'), xMax: 0, yMax: 0, x: 0, y: 0 };
+var mouse = { x: 0, y: 0 };
+var vw = 0, vh = 0;
+var targetX = 0, targetY = 0;
 
 function init() {
   resize();
   img.element.style.transform = 'translate(0px, 0px)';
-
   window.requestAnimationFrame(moveImage);
   window.addEventListener('mousemove', moveAction);
   window.addEventListener('touchmove', moveAction);
   window.addEventListener('resize', resize);
 }
 
-function map(x, a, b, c, d) {
-  return c + ((d - c) * (x - a)) / (b - a) || 0;
-}
-
-function resize() {
-  vw = window.innerWidth;
-  vh = window.innerHeight;
-  img.xMax = vw - img.element.naturalWidth;
-  img.yMax = vh - img.element.naturalHeight;
-}
+function map(x, a, b, c, d) { return c + ((d - c) * (x - a)) / (b - a) || 0; }
+function resize() { vw = window.innerWidth; vh = window.innerHeight; img.xMax = vw - img.element.naturalWidth; img.yMax = vh - img.element.naturalHeight; }
 
 function moveImage() {
   img.x = lerp(img.x, targetX, acceleration);
   img.y = lerp(img.y, targetY, acceleration);
-
-  var posX = img.x + 'px';
-  var posY = img.y + 'px';
-  img.element.style.transform = 'translate(' + posX + ', ' + posY + ')';
-
+  img.element.style.transform = 'translate(' + img.x + 'px, ' + img.y + 'px)';
   window.requestAnimationFrame(moveImage);
 }
 
 function moveAction(event) {
-  if (event.targetTouches && event.targetTouches[0]) {
-    event.preventDefault();
-    mouse.x = event.targetTouches[0].clientX;
-    mouse.y = event.targetTouches[0].clientY;
-  } else {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
-  }
-
+  var touch = event.targetTouches && event.targetTouches[0];
+  mouse.x = touch ? touch.clientX : event.clientX;
+  mouse.y = touch ? touch.clientY : event.clientY;
   targetX = map(mouse.x, 0, vw, 0, img.xMax);
   targetY = map(mouse.y, 0, vh, 0, img.yMax);
 }
 
-function lerp(start, end, t) {
-  return start * (1 - t) + end * t;
-}
+function lerp(start, end, t) { return start * (1 - t) + end * t; }
 
 window.addEventListener('load', init);
 
 /***************************************** Tooltip *****************************************/
 const areas = document.getElementsByTagName('area');
+
 for (let i = 0; i < areas.length; i++) {
+  const tooltip = document.createElement('div');
+  tooltip.id = 'tooltip';
+  tooltip.style.position = 'absolute';
+
   areas[i].addEventListener('mouseover', function(event) {
-    const tooltip = document.createElement('div');
-    tooltip.setAttribute('id', 'tooltip');
     tooltip.innerHTML = this.getAttribute('title');
-    tooltip.style.position = 'absolute';
     document.body.appendChild(tooltip);
     updateTooltipPosition(event, tooltip);
+
     window.addEventListener('mousemove', function(event) {
       updateTooltipPosition(event, tooltip);
     });
+
     this.removeAttribute('title');
   });
+
   areas[i].addEventListener('mouseout', function() {
-    const tooltip = document.querySelector('#tooltip');
     tooltip.parentNode.removeChild(tooltip);
     this.setAttribute('title', tooltip.innerHTML);
   });
 }
 
 function updateTooltipPosition(event, tooltip) {
-  tooltip.style.top = (event.pageY + 10) + 'px';
-  tooltip.style.left = (event.pageX + 10) + 'px';
+  tooltip.style.top = `${event.pageY + 10}px`;
+  tooltip.style.left = `${event.pageX + 10}px`;
 }
 
 /***************************************** Cookie Notice *****************************************/
 const cookieNotice = document.createElement('div');
-cookieNotice.setAttribute('id', 'cookie-notice');
+cookieNotice.id = 'cookie-notice';
 cookieNotice.innerHTML = `<p>We use cookies. <a href="https://www.iubenda.com/privacy-policy/86096520" target="_blank" rel="noreferrer">Read Policy.</a> <a onclick="acceptCookies()">Accept.</a></p>`;
 document.body.appendChild(cookieNotice);
 
-function setCookie(cookieName, cookieValue, expirationDays) {
+const acceptCookies = () => {
+  setCookie('HHcookienotice', 'true', 365);
+  cookieNotice.style.display = 'none';
+};
+
+const setCookie = (cookieName, cookieValue, expirationDays) => {
   const d = new Date();
-  d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+  d.setTime(d.getTime() + expirationDays * 24 * 60 * 60 * 1000);
   const expires = 'expires=' + d.toUTCString();
   document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
-}
+};
 
-function getCookie(cookieName) {
+const getCookie = (cookieName) => {
   const name = `${cookieName}=`;
   const decodedCookie = decodeURIComponent(document.cookie);
   const cookieArray = decodedCookie.split(';');
   for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1);
-    }
+    let cookie = cookieArray[i].trim();
     if (cookie.indexOf(name) === 0) {
       return cookie.substring(name.length, cookie.length);
     }
   }
   return '';
-}
-
-function acceptCookies() {
-  setCookie('HHcookienotice', 'true', 365);
-  const cookieNoticeDiv = document.getElementById('cookie-notice');
-  cookieNoticeDiv.style.display = 'none';
-}
+};
 
 if (getCookie('HHcookienotice') === 'true') {
-  const cookieNoticeDiv = document.getElementById('cookie-notice');
-  cookieNoticeDiv.style.display = 'none';
+  cookieNotice.style.display = 'none';
 }
