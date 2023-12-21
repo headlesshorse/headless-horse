c54f66c7a931e91b6a33867045d77200ce33b00c
Headless Horse <github-login@headless.horse>
2023-12-21 13:54:08 +0000

    5.0

--- assets/js/carbon.js
+++ assets/js/carbon.js
@@ -1,6 +1,7 @@
 const displayCarbonData = async () => {
-  const apiUrl = `https://digitalbeacon.co/badge?url=${encodeURIComponent(currentPageUrl)}`
+  const apiUrl = `https://digitalbeacon.co/badge?url=${encodeURIComponent(window.location.href)}`;
   const { size = '', url = '', co2 = '' } = await (await fetch(apiUrl)).json();
   document.getElementById('carbon').outerHTML = `<a href="${url}" target="_blank" data-more="Low-consumption site using renewable energy.">${size} / ${co2}</a>`;
 };
+
 displayCarbonData();
