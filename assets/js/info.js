a39baff45c111a2c59ac94d950aafa737c89723f
Headless Horse <github-login@headless.horse>
2024-01-08 14:31:26 +0000

    5.0

--- assets/js/info.js
+++ assets/js/info.js
@@ -2,8 +2,8 @@ const fetchProjectData = async () => {
   const { projects } = await (await fetch('/assets/data/projects.json')).json();
   const sortedPressLinks = projects.flatMap(project => (project.press || []).map(link => ({ ...link, projectName: project.title })))
     .slice().sort((a, b) => a.title.localeCompare(b.title));
-
-  const pressTemplate = `
+  
+  const template = `
     <section>
       <h2>Press</h2>
       <ul>
@@ -11,32 +11,37 @@ const fetchProjectData = async () => {
       </ul>
     </section>
   `;
-
-  document.querySelector('section').parentNode.insertAdjacentHTML('beforeend', pressTemplate);
-
-  // Typing effect
+  document.querySelector('section').parentNode.insertAdjacentHTML('beforeend', template);
+  
+  // Typing
   document.querySelectorAll("article *").forEach(element => {
     if (element.childNodes.length === 1 && element.childNodes[0].nodeType === 3 && !element.classList.contains("typewriter")) {
       element.classList.add("typewriter");
       typeWriter(element, 120);
     }
   });
+
+  // Carbon
+  const displayCarbonData = async () => {
+    const apiUrl = `https://digitalbeacon.co/badge?url=${encodeURIComponent(window.location.href)}`;
+    const { size = '', url = '', co2 = '' } = await (await fetch(apiUrl)).json();
+    document.getElementById('carbon').outerHTML = `<a href="${url}" target="_blank" data-more="Low-consumption site using renewable energy.">${size} / ${co2}</a>`;
+  };  
+
+  displayCarbonData();
 };
 
-window.onload = () => {
-  fetchProjectData().then(() => {
-    displayCarbonData();
-  });
+window.onload = fetchProjectData;
 
-  // Hours
-  const now = new Date();
-  const day = now.getUTCDay();
-  const hour = now.getUTCHours() + 1;
-  const isOpen = (day === 0 || day === 6) || (day === 5 && hour >= 18) ? 'We are out of office and will return Monday morning.' : (hour >= 9 && hour < 18) ? 'The studio is open today from 09:00-18:00.' : 'We are out of office. We will be open tomorrow, 09:00-18:00.';
-  document.querySelector('p').insertAdjacentHTML('beforeend', '<br>' + isOpen);
-};
+// Hours
+const now = new Date();
+const day = now.getUTCDay();
+const hour = now.getUTCHours() + 1;
+const isOpen = (day === 0 || day === 6) || (day === 5 && hour >= 18) ? 'We are out of office and will return Monday morning.' : (hour >= 9 && hour < 18) ? 'The studio is open today from 09:00-18:00.' : 'We are out of office. We will be open tomorrow, 09:00-18:00.';
 
-// Typing effect
+document.querySelector('p').insertAdjacentHTML('beforeend', '<br>' + isOpen);
+
+// Typing
 function typeWriter(element, speed) {
   let text = element.textContent;
   element.textContent = text.substring(0, text.length - 20);
@@ -50,10 +55,3 @@ function typeWriter(element, speed) {
   }
   type();
 }
-
-// Carbon data
-const displayCarbonData = async () => {
-  const apiUrl = `https://digitalbeacon.co/badge?url=${encodeURIComponent(window.location.href)}`;
-  const { size = '', url = '', co2 = '' } = await (await fetch(apiUrl)).json();
-  document.getElementById('carbon').outerHTML = `<a href="${url}" target="_blank" data-more="Low-consumption site using renewable energy.">${size} / ${co2}</a>`;
-};
--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,17 +4,15 @@ async function fetchProjectData() {
   const project = projects.find(({ slug }) => slug === currentPage);
 
   document.title = `${project.title} | HEADLESS HORSE`;
-  populateHTML(project, projects).then(() => {
-    displayCarbonData();
-  });
+  populateHTML(project, projects);
 }
 
 const getSizeInKB = async (url) => Math.ceil((await fetch(url).then(res => res.blob())).size / 1024);
 
 const populateHTML = async (project, allProjects) => {
+
   let mediaListHTML = '';
   const mediaItemsHTML = [];
-
   for (let index = 0; index < project.media.length; index++) {
     const { src, alt, type } = project.media[index];
     const sizeKB = await getSizeInKB(src);
@@ -26,64 +24,73 @@ const populateHTML = async (project, allProjects) => {
     const linkNumber = (index + 1).toString().padStart(2, '0');
     const listItem = `<li><a href="#media-${linkNumber}" data-more="${type} / ${sizeKB} KB">${linkNumber}. ${alt}</a></li>`;
     mediaListHTML += listItem;
-
+  
     const figureItem = `<figure id="media-${linkNumber}">${mediaContent}<figcaption>${linkNumber}. ${alt} / ${sizeKB} KB</figcaption></figure>`;
     mediaItemsHTML.push(figureItem);
   }
-
+  
   const getNextProjectLink = (nextProject) => nextProject ? `<a href="/projects/${nextProject.slug}">[Next: ${nextProject.title}]</a>` : '';
 
   const template = `
-  <main>
-    <article id="project--details">
-      <section>
-        <h2>${project.title}</h2>
-        <p>${project.description}</p>
-      </section>
-      <section>
-        ${project.links.length > 0 ? '<h2>Links</h2>' : ''}
-        <ul>${project.links.map(({ link, title }) => `
-          <li><a href="${link}" target="_blank" rel="noreferrer" data-more="${link}">${title}</a></li>`).join('')}
-        </ul>
-        ${project.press.length > 0 ? '<h2>Press</h2>' : ''}
-        <ul>${project.press.map(({ link, title, date }) => `
-          <li><a href="${link}" target="_blank" rel="noreferrer" data-more="${date}">${title}</a></li>`).join('')}
-        </ul>
-        ${project.credits.length > 0 ? '<h2>Credits</h2>' : ''}
-        <ul>${project.credits.map(({ link, title, credit }) => `
-          <li><a href="${link}" target="_blank" rel="noreferrer" data-more="${credit}">${title}</a></li>`).join('')}
-        </ul>
-      </section>
-      <details>
-        <summary>[INDEX]</summary>
-        <ul>${mediaListHTML}</ul>
-      </details>
-    </article>
+    <main>
+      <article id="project--details">
+        <section>
+          <h2>${project.title}</h2>
+          <p>${project.description}</p>
+        </section>
+        <section>
+          ${project.links.length > 0 ? '<h2>Links</h2>' : ''}
+          <ul>${project.links.map(({ link, title }) => `
+            <li><a href="${link}" target="_blank" rel="noreferrer" data-more="${link}">${title}</a></li>`).join('')}
+          </ul>
+          ${project.press.length > 0 ? '<h2>Press</h2>' : ''}
+          <ul>${project.press.map(({ link, title, date }) => `
+            <li><a href="${link}" target="_blank" rel="noreferrer" data-more="${date}">${title}</a></li>`).join('')}
+          </ul>
+          ${project.credits.length > 0 ? '<h2>Credits</h2>' : ''}
+          <ul>${project.credits.map(({ link, title, credit }) => `
+            <li><a href="${link}" target="_blank" rel="noreferrer" data-more="${credit}">${title}</a></li>`).join('')}
+          </ul>
+        </section>
+        <details>
+          <summary>[INDEX]</summary>
+          <ul>${mediaListHTML}</ul>
+        </details>
+      </article>
 
-    <section id="project--media">
-      ${mediaItemsHTML.join('')}
-    </section>
+      <section id="project--media">
+        ${mediaItemsHTML.join('')}
+      </section>
 
-    <footer>
-      <a id="carbon"></a><a id="next"${getNextProjectLink(allProjects[(allProjects.findIndex(({ slug }) => slug === project.slug) + 1) % allProjects.length])}</a>
-    </footer>
-  </main> 
+      <footer>
+          <a id="carbon"></a><a id="next"${getNextProjectLink(allProjects[(allProjects.findIndex(({ slug }) => slug === project.slug) + 1) % allProjects.length])}</a>
+      </footer>
+    </main>
   `;
 
   document.querySelector('nav').insertAdjacentHTML('afterend', template);
-  
-  // Typing effect
+
+  // Typing
   document.querySelectorAll("article h2, article p, article a").forEach(element => {
     if (element.childNodes.length === 1 && element.childNodes[0].nodeType === 3 && !element.classList.contains("typewriter")) {
       element.classList.add("typewriter");
       typeWriter(element, 120);
     }
   });
+
+  // Carbon
+  const displayCarbonData = async () => {
+    const apiUrl = `https://digitalbeacon.co/badge?url=${encodeURIComponent(window.location.href)}`;
+    const { size = '', url = '', co2 = '' } = await (await fetch(apiUrl)).json();
+    document.getElementById('carbon').outerHTML = `<a href="${url}" target="_blank" data-more="Low-consumption site using renewable energy.">${size} / ${co2}</a>`;
+  };  
+  
+  displayCarbonData();
 };
 
 window.onload = fetchProjectData;
 
-// Typing effect
+// Typing
 function typeWriter(element, speed) {
   let text = element.textContent;
   element.textContent = text.substring(0, text.length - 20);
@@ -97,10 +104,3 @@ function typeWriter(element, speed) {
   }
   type();
 }
-
-// Carbon data
-const displayCarbonData = async () => {
-  const apiUrl = `https://digitalbeacon.co/badge?url=${encodeURIComponent(window.location.href)}`;
-  const { size = '', url = '', co2 = '' } = await (await fetch(apiUrl)).json();
-  document.getElementById('carbon').outerHTML = `<a href="${url}" target="_blank" data-more="Low-consumption site using renewable energy.">${size} / ${co2}</a>`;
-};
