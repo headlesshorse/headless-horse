fec574bccaf90899fb4fcd9947ae9b9f6b8f6a58
Headless Horse <github-login@headless.horse>
2024-06-16 17:24:58 +0100

    5.0

--- assets/js/project.js
+++ assets/js/project.js
@@ -7,31 +7,24 @@ async function fetchProjectData() {
 	populateHTML(project, projects);
 }
 
-const getSizeInKB = async (url) => Math.ceil((await fetch(url).then(res => res.blob())).size / 1024);
+const getSizeInKB = async (url) => Math.ceil((await (await fetch(url)).blob()).size / 1024);
 
 const populateHTML = async (project, allProjects) => {
-
 	let mediaListHTML = '';
 	const mediaItemsHTML = [];
+
 	for (let index = 0; index < project.media.length; index++) {
-		const { src, alt, type } = project.media[index];
+		const { src, alt } = project.media[index];
 		const sizeKB = await getSizeInKB(src);
 
-		const mediaContent = type === 'Image'
-			? `<img src="${src}" alt="${alt}" loading="lazy" width="auto" height="100%">`
-			: `<video autoplay loop muted preload="none" width="auto" height="100%"><source src="${src}" type="video/mp4"></video>`;
-
 		const linkNumber = (index + 1).toString().padStart(2, '0');
-		const listItem = `<li><a href="#media-${linkNumber}" data-more="${type} / ${sizeKB} KB">${linkNumber}. ${alt}</a></li>`;
-		mediaListHTML += listItem;
-
-		const figureItem = `<figure id="media-${linkNumber}">${mediaContent}<figcaption>${linkNumber}. ${alt} / ${sizeKB} KB</figcaption></figure>`;
-		mediaItemsHTML.push(figureItem);
+		mediaListHTML += `<li><a href="#media-${linkNumber}" data-more="Image / ${sizeKB} KB">${linkNumber}. ${alt}</a></li>`;
+		mediaItemsHTML.push(`<figure id="media-${linkNumber}"><img src="${src}" alt="${alt}" loading="lazy" width="100%" height="100%"><figcaption>${linkNumber}. ${alt} / ${sizeKB} KB</figcaption></figure>`);
 	}
 
 	const getNextProjectLink = (nextProject) => nextProject ? `<a href="/projects/${nextProject.slug}">[Next: ${nextProject.title}]</a>` : '';
 
-	const template = `
+	document.querySelector('header').insertAdjacentHTML('afterend', `
     <main>
       <article id="details">
         <section>
@@ -39,28 +32,22 @@ const populateHTML = async (project, allProjects) => {
           <p>${project.description}</p>
         </section>
         <section>
-          ${project.links.length > 0 ? '<h2>Links</h2><ul>' + project.links.map(({ link, title }) => `<li><a href="${link}" target="_blank" rel="noreferrer" data-more="${link}">${title}</a></li>`).join('') + '</ul>' : ''}
-          ${project.press.length > 0 ? '<h2>Press</h2><ul>' + project.press.map(({ link, title, date }) => `<li><a href="${link}" target="_blank" rel="noreferrer" data-more="${date}">${title}</a></li>`).join('') + '</ul>' : ''}
-          ${project.credits.length > 0 ? '<h2>Credits</h2><ul>' + project.credits.map(({ link, title, credit }) => `<li><a href="${link}" target="_blank" rel="noreferrer" data-more="${credit}">${title}</a></li>`).join('') + '</ul>' : ''}
+          ${project.links.length ? `<h2>Links</h2><ul>${project.links.map(({ link, title }) => `<li><a href="${link}" target="_blank" rel="noreferrer" data-more="${link}">${title}</a></li>`).join('')}</ul>` : ''}
+          ${project.press.length ? `<h2>Press</h2><ul>${project.press.map(({ link, title, date }) => `<li><a href="${link}" target="_blank" rel="noreferrer" data-more="${date}">${title}</a></li>`).join('')}</ul>` : ''}
+          ${project.credits.length ? `<h2>Credits</h2><ul>${project.credits.map(({ link, title, credit }) => `<li><a href="${link}" target="_blank" rel="noreferrer" data-more="${credit}">${title}</a></li>`).join('')}</ul>` : ''}
         </section>
         <details>
           <summary>[Index]</summary>
           <ul>${mediaListHTML}</ul>
         </details>
       </article>
-
-      <section id="media">
-        ${mediaItemsHTML.join('')}
-      </section>
-
+      <section id="media">${mediaItemsHTML.join('')}</section>
       <footer>
         <a id="carbon"></a>
-        <a "${getNextProjectLink(allProjects[(allProjects.findIndex(({ slug }) => slug === project.slug) + 1) % allProjects.length])}</a>
+        ${getNextProjectLink(allProjects[(allProjects.findIndex(({ slug }) => slug === project.slug) + 1) % allProjects.length])}
       </footer>
     </main>
-  `;
-
-	document.querySelector('header').insertAdjacentHTML('afterend', template);
+  `);
 };
 
 window.onload = fetchProjectData;
