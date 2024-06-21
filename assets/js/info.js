60a71ad2f9c4433a66a0e10c24990c791676edd4
Headless Horse <github-login@headless.horse>
2024-06-21 11:06:38 +0100

    5.0

--- assets/js/info.js
+++ assets/js/info.js
@@ -1,13 +1,12 @@
-// Press
-const fetchProjectData = async () => {
-  const { projects } = await (await fetch('/assets/data/projects.json')).json();
+const fetchWorkData = async () => {
+  const { works } = await (await fetch('/assets/data/work.json')).json();
 
   const titleCounts = {};
-  const pressLinks = projects.flatMap(project => (project.press || []).map(link => {
+  const pressLinks = works.flatMap(work => (work.press || []).map(link => {
     const title = link.title.toUpperCase();
     titleCounts[title] = (titleCounts[title] || 0) + 1;
     const sectionLink = titleCounts[title] > 1 ? ` / ${titleCounts[title]}` : '';
-    return { ...link, projectName: project.title, sectionLink, firstOccurrence: titleCounts[title] === 1 };
+    return { ...link, projectName: work.title, sectionLink, firstOccurrence: titleCounts[title] === 1 };
   }));
 
   const pressGroups = pressLinks.reduce((groups, link) => {
@@ -22,23 +21,24 @@ const fetchProjectData = async () => {
     <section>
       <h2>Press</h2>
       <ul>
-        ${sortedPressGroups.map(title => {
-          const group = pressGroups[title];
-          return `<li>${group.map((link, index) => `<a href="${link.link}" target="_blank" rel="noreferrer" data-more="${link.projectName}, ${link.date}">${index === 0 ? link.title : ''}${link.sectionLink}</a>`).join('')}</li>`;
-        }).join('')}
+        ${sortedPressGroups.map(title => `<li>${pressGroups[title].map((link, index) => `<a href="${link.link}" target="_blank" rel="noreferrer" data-more="${link.projectName}, ${link.date}">${index === 0 ? link.title : ''}${link.sectionLink}</a>`).join('')}</li>`).join('')}
       </ul>
     </section>
   `;
 
   document.querySelector('section').parentNode.insertAdjacentHTML('beforeend', template);
 };
-
-window.onload = fetchProjectData;
+window.onload = fetchWorkData;
 
 // Hours
 const now = new Date();
 const day = now.getUTCDay();
 const hour = now.getUTCHours() + 1;
-const isOpen = (day === 0 || day === 6) || (day === 5 && hour >= 18) ? 'We are out of office and will return Monday morning.' : (hour >= 9 && hour < 18) ? 'The studio is open today from 09:00-18:00.' : 'We are out of office. We will be open tomorrow, 09:00-18:00.';
+
+const isOpen = 
+  (day === 0 || day === 6) || 
+  (day === 5 && hour >= 18) ? 'We are out of office and will return Monday morning.' : 
+  (hour >= 9 && hour < 18) ? 'The studio is open today from 09:00-18:00.' : 
+  'We are out of office. We will be open tomorrow, 09:00-18:00.';
 
 document.querySelector('p').insertAdjacentHTML('beforeend', '<br>' + isOpen);
