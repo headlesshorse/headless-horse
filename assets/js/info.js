74b34dcd4f8872fbad465f4692d54931503bc67b
HEADLESS HORSE <github-login@headless.horse>
2024-07-16 17:45:09 +0100

    5.0

--- assets/js/info.js
+++ assets/js/info.js
@@ -6,29 +6,22 @@ const fetchWorkData = async () => {
   const pressLinks = works.flatMap(work => (work.press || []).map(link => {
     const title = link.title.toUpperCase();
     titleCounts[title] = (titleCounts[title] || 0) + 1;
-    const sectionLink = titleCounts[title] > 1 ? ` / ${titleCounts[title]}` : '';
-    return { ...link, projectName: work.title, sectionLink, firstOccurrence: titleCounts[title] === 1 };
+    return { ...link, projectName: work.title, sectionLink: titleCounts[title] > 1 ? ` / ${titleCounts[title]}` : '', firstOccurrence: titleCounts[title] === 1 };
   }));
 
   const pressGroups = pressLinks.reduce((groups, link) => {
-    groups[link.title] = groups[link.title] || [];
-    groups[link.title].push(link);
+    (groups[link.title] = groups[link.title] || []).push(link);
     return groups;
   }, {});
 
-  const sortedPressGroups = Object.keys(pressGroups).sort();
-
   const template = `
-    <section>
-      <h2>Press</h2>
-      <ul>
-        ${sortedPressGroups.map(title => `<li>${pressGroups[title].map((link, index) => `<a href="${link.link}" target="_blank" rel="noreferrer" data-more="${link.projectName}, ${link.date}">${index === 0 ? link.title : ''}${link.sectionLink}</a>`).join('')}</li>`).join('')}
-      </ul>
-    </section>
+    <h2>Press</h2>
+    <ul>${Object.keys(pressGroups).sort().map(title => `<li>${pressGroups[title].map((link, index) => `<a href="${link.link}" target="_blank" rel="noreferrer" data-more="${link.projectName}, ${link.date}">${index === 0 ? link.title : ''}${link.sectionLink}</a>`).join('')}</li>`).join('')}</ul>
+    <br>
   `;
-
-  document.querySelector('section').parentNode.insertAdjacentHTML('beforeend', template);
+  document.querySelector('section:last-of-type').insertAdjacentHTML('afterbegin', template);
 };
+
 window.onload = fetchWorkData;
 
 // Hours
